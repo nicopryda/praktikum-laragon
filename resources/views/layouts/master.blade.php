188c2122ee0452f31ef2eaef01ff5a1d6474a23c
nicopryda <nicoprayuda775@gmail.com>
2024-11-09 11:17:28 +0800

    komentar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>inventaris-Dashboard</title>
+    @notifyCss
 
     <!-- Custom fonts for this template-->
     <link href="/sbadmin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -51,6 +52,11 @@
                 <a class="nav-link" href="/admin/barang">
                     <i class="fas fa-fw fa-list"></i>
                     <span>Data Barang</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/admin/laporan">
+                    <i class="fas fa-fw fa-list"></i>
+                    <span>Laporan</span></a>
             </li>
 
             <!-- Divider -->
@@ -220,7 +226,8 @@
     <!-- Page level custom scripts -->
     <script src="/sbadmin/js/demo/chart-area-demo.js"></script>
     <script src="/sbadmin/js/demo/chart-pie-demo.js"></script>
-
+    <x-notify::notify />
+    @notifyJs
 </body>
 
 </html>
